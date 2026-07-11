a723c57cb40741d27e8905b0ba43110568837ae6
Anshali <anshali.manoharan.12263.11i@gmail.com>
2026-07-11 10:56:03 +0400

    removed about

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/NavBar";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
@@ -8,7 +7,6 @@ import Awards from "@/components/Awards";
 import Cursor from "@/components/Cursor";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-// import Publications from "@/components/Publications";
 import Certifications from "@/components/Certifications";
 
 export default function HomePage() {
